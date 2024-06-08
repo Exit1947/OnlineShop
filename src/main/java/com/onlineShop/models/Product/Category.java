679fc0954e2844bb14c354b148590b4403ccda96
Exit1947 <speccore1947@gmail.com>
2024-06-08 11:25:45 +0300

    Editing error, inheritance is possible for Product, not for Category.

--- src/main/java/com/onlineShop/models/Product/Category.java
+++ src/main/java/com/onlineShop/models/Product/Category.java
@@ -10,7 +10,6 @@ import org.springframework.lang.NonNull;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Category {
     @Id
     @NonNull
