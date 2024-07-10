7e6a0919ca2f0419183cf817d22b4f82dd311887
Exit1947 <speccore1947@gmail.com>
2024-07-10 16:26:33 +0300

    Adding new roles for base db initializing.

--- src/main/resources/sqlInitialize/roleInitialize.sql
+++ src/main/resources/sqlInitialize/roleInitialize.sql
@@ -1,4 +1,7 @@
+INSERT INTO role (type) VALUES ('super_admin');
 INSERT INTO role (type) VALUES ('admin');
+INSERT INTO role (type) VALUES ('super_moderator');
 INSERT INTO role (type) VALUES ('moderator');
+INSERT INTO role (type) VALUES ('super_sales_rep');
 INSERT INTO role (type) VALUES ('sales_rep');
 INSERT INTO role (type) VALUES ('end_user');
