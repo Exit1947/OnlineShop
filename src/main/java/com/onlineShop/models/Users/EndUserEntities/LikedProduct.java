d1150c82196d88717ad45f3e4ddcec8c20213966
Exit1947 <speccore1947@gmail.com>
2024-07-10 19:17:22 +0300

    Refactoring code for LikedProduct.

--- src/main/java/com/onlineShop/models/Users/EndUserEntities/LikedProduct.java
+++ src/main/java/com/onlineShop/models/Users/EndUserEntities/LikedProduct.java
@@ -2,6 +2,8 @@ package com.onlineShop.models.Users.EndUserEntities;
 
 import com.onlineShop.models.Product.Product;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +17,16 @@ import org.springframework.lang.NonNull;
 @NoArgsConstructor
 public class LikedProduct {
     @Id
-    @NonNull
+    @NotBlank
     @Column(name = "id")
     private String id;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="id_user")
     private EndUser user;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name="id_product")
     private Product product;
