d2a36ac002f13fc899ad14662c5011e9f0238eb3
Mari-ymi <ymi19@ukr.net>
2024-09-16 23:04:11 +0300

    Changes css 16.09.2024

--- frontend/src/components/logInPage/LogPage.tsx
+++ frontend/src/components/logInPage/LogPage.tsx
@@ -1,9 +1,11 @@
 import  './loginStyle.css'
 import logoPage from '../../img/Group 1474.png';
+import FitImage from 'react-native-fit-image';
 
 
 
 const LoginPage =()=>{
+      
 
     return (
 
@@ -11,8 +13,12 @@ const LoginPage =()=>{
              
             <div className='logo-box'>
 
+                <div className='top-box'>
+
                 <div className='box-image'  >
-                <img src={logoPage} alt="" className='paint' ></img>
+                
+                    <img src={logoPage} alt="" className='paint'  />
+                
                 </div>
 
 
@@ -20,7 +26,7 @@ const LoginPage =()=>{
 
 
                 <div className='box-title'>            
-                <h1>Sign in</h1>
+                <div className='sign'>Sign in</div>
                  </div>
 
             <div className='box-input'>            
@@ -28,13 +34,23 @@ const LoginPage =()=>{
                 <label><strong>Email or mobile phone number</strong></label>
                 <input type="text" placeholder="" className="input-number"/>
                 <div className='between'></div>
-                <label>Password</label>
+                <label>P a s s w o r d</label>
                 <input type="password" placeholder="" className="input-password"/>
                 <div className='between'></div>                
                 
-                <button type="submit" className="btn-confirm">Login</button>
+                <button type="submit" className="btn-confirm">L o g i n</button>
             </form>
             </div>
+
+            </div>
+
+            <div className='newOmnify'>
+                <hr className='line-under'></hr>
+                <div className='new-omnify'>New to Omnify?</div>
+                <hr className='line-after'></hr>
+            </div>
+
+            <button type="submit" className="btn-create-account">Create your Omnify account</button>
             </div>
             </div>
         </div>
