983fd68937d10dc16daf6bff297d64350dfd3cff
Exit1947 <speccore1947@gmail.com>
2024-11-25 11:03:19 +0200

    Adding new files for working with orderedProducts: repo, service and serviceImpl.

---
+++ backend/src/main/java/com/onlineShop/repository/OrderedProductsRepository.java
@@ -0,0 +1,17 @@
+package com.onlineShop.repository;
+
+import com.onlineShop.models.Users.EndUserEntities.shoppingOrder.OrderedProducts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderedProductsRepository extends JpaRepository<OrderedProducts, Long> {
+
+    Optional<OrderedProducts> findById(long id);
+
+    List<OrderedProducts> findAllByProductId(String productId);
+
+}
---
+++ backend/src/main/java/com/onlineShop/service/serviceImpl/OrderedProductsServiceImpl.java
@@ -0,0 +1,62 @@
+package com.onlineShop.service.serviceImpl;
+
+import com.onlineShop.models.Users.EndUserEntities.shoppingOrder.OrderedProducts;
+import com.onlineShop.repository.OrderedProductsRepository;
+import com.onlineShop.service.OrderedProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderedProductsServiceImpl implements OrderedProductsService {
+
+    private final OrderedProductsRepository orderedProductsRepository;
+
+    @Autowired
+    public OrderedProductsServiceImpl(OrderedProductsRepository orderedProductsRepository) {
+        this.orderedProductsRepository = orderedProductsRepository;
+    }
+
+    @Override
+    public boolean save(OrderedProducts orderedProducts) {
+        orderedProductsRepository.save(orderedProducts);
+        return true;
+    }
+
+    @Override
+    public Optional<OrderedProducts> findById(long id) {
+        return orderedProductsRepository.findById(id);
+    }
+
+    @Override
+    public List<OrderedProducts> findAllByProductId(String productId) {
+        return orderedProductsRepository.findAllByProductId(productId);
+    }
+
+    @Override
+    public boolean update(OrderedProducts orderedProducts) {
+        Optional<OrderedProducts> existingOrderedProducts = orderedProductsRepository.findById(orderedProducts.getId());
+        existingOrderedProducts.ifPresent(products -> orderedProducts.setId(products.getId()));
+        orderedProductsRepository.save(orderedProducts);
+        return true;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        if(orderedProductsRepository.existsById(id)) {
+            orderedProductsRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteAll(List<OrderedProducts> orderedProductsList) {
+        orderedProductsList.
+                forEach(orderedProduct -> delete(orderedProduct.getId()));
+        return true;
+    }
+
+}
