ae40fd07972e9a5e955713970b928f5896ba6480
Exit1947 <speccore1947@gmail.com>
2024-12-01 19:27:42 +0200

    Adding entity card and card item.

---
+++ backend/src/main/java/com/onlineShop/models/Users/EndUserEntities/cart/Item.java
@@ -0,0 +1,38 @@
+package com.onlineShop.models.Users.EndUserEntities.cart;
+
+import com.onlineShop.models.Product.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cart")
+    private Cart cart;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_product")
+    private Product product;
+
+    @Positive(message = "Count must be positive number")
+    @Column(name = "count_of_products")
+    private int countOfProducts;
+
+}
