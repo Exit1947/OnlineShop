2820f8970ad166a4c68ad0fecdc604cbf417f0a2
Exit1947 <speccore1947@gmail.com>
2024-12-08 18:48:06 +0200

    Refactoring item repo and adding gateways

--- backend/src/main/java/com/onlineShop/repository/ItemRepository.java
+++ backend/src/main/java/com/onlineShop/repository/ItemRepository.java
@@ -3,12 +3,8 @@ package com.onlineShop.repository;
 import com.onlineShop.models.Users.EndUserEntities.cart.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     void deleteItemById(Long id);
-
-    void deleteAll(List<Item> items);
 
 }
